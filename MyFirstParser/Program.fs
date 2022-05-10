8197fb6ce7986e324e2956999b3bc12e022118b8
Kasper <kasper35@live.no>
2022-05-10 20:19:14 +0200

    Yes sir!

---
+++ MyFirstParser/Program.fs
@@ -0,0 +1,18 @@
+﻿open MyFirstParser.Query
+open MyFirstParser.Books
+
+let input = """
+filterby Category = 'Fantasy'
+orderby Rating asc 
+take 1
+"""
+let books = getAll()
+let result = parse input 
+
+match result with 
+  | Result.Ok res -> 
+    let queryResult = execute res books 
+    List.iter (fun i -> printfn "%O" i) queryResult
+  | Result.Error res -> printfn "%O" res
+
+
